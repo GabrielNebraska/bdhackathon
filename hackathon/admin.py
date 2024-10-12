15d4f7b3c6b59cdeb704d8f101701dcb6fc0ee43
GabrielNebraska <gabrielviniciuscorrea204@gmail.com>
2024-10-12 16:37:06 -0300

    Tutorial Part 4.6

--- hackathon/admin.py
+++ hackathon/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Cargo
+from .models import Cargo, funcionario
 
 admin.site.register(Cargo)
+admin.site.register(funcionario)
