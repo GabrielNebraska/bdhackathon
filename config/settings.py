e074b79e5394974f7cecf8736b936885654a344c
GabrielNebraska <gabrielviniciuscorrea204@gmail.com>
2024-11-13 14:58:15 -0300

    feat: fiz coisas

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "uploader",
     "hackathon",
-    "swagger:drf-yasg",
+    "drf-yasg",
     "rest_framework_swagger",
 
 ]
--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from uploader.router import router as uploader_router
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework_swagger import schema_view
+from rest_framework.swagger import schema_view
 from rest_framework.routers import DefaultRouter
 from hackathon.views import CargoViewSet, UsuarioViewSet, ClienteViewSet, OrcamentosViewSet, ServicoViewSet, VerificacoespreventivasViewSet, RelatorioViewSet, EstoqueViewSet, FerramentaspecasViewSet, ParalelismoViewSet, FuncionarioViewSet, AdministradorViewSet
 
@@ -33,5 +33,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
